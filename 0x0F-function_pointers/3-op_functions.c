a3ab99a9af77fc82b9cd3d213939e659a594605f
root <RobinLeeHenry101@gmail.com>
2023-06-21 06:09:44 -0700

    finished everything and tested it

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - this func returns the sum of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - this func returns the difference of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - this func returns the product of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - this func returns the division of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - this func returns the remainder of the division of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
